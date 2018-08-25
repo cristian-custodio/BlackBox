a216eba63a9cc85322d8a833f75dde71533100bd
Joe Ragno <joseph.ragno3@gmail.com>
2018-08-25 13:39:08 -0400

    added helpers folder and some authentication

--- models/user.js
+++ models/user.js
@@ -52,8 +52,28 @@ module.exports = function(sequelize, DataTypes) {
     // Users joinDate, type DATEONLY
     joinDate: {
       type: DataTypes.DATEONLY
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique: true,
+        validate:{
+            isEmail: true
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull:false
     }
   });
+
+  User.prototype.validPassword = function(password){
+      return bcrypt.compareSync(password,this.password);
+  }
+
+  User.hook("beforeCreate", function(user){
+      user.password= bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  })
 
   return User;
 };
