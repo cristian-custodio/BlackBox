f10b5e7fd6b1a276b52e79f41a6037a0e0f51b2d
Joe Ragno <joseph.ragno3@gmail.com>
2018-08-25 10:11:56 -0400

    fixed database to sequelize

--- models/savings.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Savings = sequelize.define("Savings", {
-    // Giving the Savings model a name of type STRING
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
---
+++ models/transactions.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+
+  var Transactions = sequelize.define("Transactions", {
+    // Giving the Transactions model an amount of type DOUBLE
+    amount: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    // Giving the Transactions model an accountid of type uuid
+    Accounts_AccountID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID3
+      len: [9,10]
+    },
+    // Giving the Transactions model a dateTime of DATE
+    dateTime: {
+      DataTypes.NOW
+    }
+  });
+
+  Transactions.associate = function(models) {
+    Transactions.belongsTO(models.Accounts, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
+  }
+
+  return Transactions;
+};
