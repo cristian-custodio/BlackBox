1d1976d36780a74c4277bef3a89e1fc623985b3f
Joseph Evangelista <oootep123@gmail.com>
2018-08-30 22:05:41 -0400

    updated put route for accounts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,101 +23,107 @@ module.exports = function (app) {
     // Put route to update user's row
     app.put("/api/user/:User_ID", function (req, res) {
         db.User.findOne({
-            where:{
+            where: {
                 User_ID: req.params.User_ID
             }
-        }).then(function(usersId){
+        }).then(function (usersId) {
             var userId = usersId;
 
-            db.User.update(req.body,userId).then(function (dbUser) {
+            db.User.update(req.body, userId).then(function (dbUser) {
                 res.json(dbUser);
             });
         });
-        })
-        
-        
-
-
-// GET route for accounts 
-app.get("/api/accounts", function (req, res) {
-    db.Account.findAll({ include: [db.Account] })
-        .then(function (dbAccounts) {
-            res.json(dbAccounts);
-        });
-});
-
-// GET route for single account
-app.get("/api/accounts/:User_ID", function (req, res) {
-    db.Account.findOne({
-        where: {
-            User_ID: req.params.User_ID
-        }
-    }).then(function (dbAccounts) {
-        res.json(dbAccounts);
-    });
-});
-
-// Put routes for updating account
-app.put("/api/accounts/:User_UserID", function (req, res) {
-    db.Account.update(req.body, {
-        where: {
-            User_ID: req.body.User_ID
-        }
-    }).then(function (dbAccounts) {
-        res.json(dbAccounts);
-    });
-});
-
-// Get route for all transactions
-app.get("/api/transactions", function (req,res){
-    db.Transaction.findAll({include:[db.Transaction]})
-}).then(function (allTransactions){
-    res.json(allTransactions);
-});
-
-// Get route for one transactions
-app.get("/api/transactions/:transactions_ID", function (req, res){
-    db.transaction.findOne({
-        where:{
-            transactions_ID: req.params.transaction_ID
-        }
-    }).then(function (oneTransaction){
-        res.json(oneTransaction);
-    });
-});
-
-
-// Post route for transactions
-app.post("/api/transactions", function (req, res) {
-
-    db.Account.findOne({
-        where: {
-            User_ID: req.params.User_ID
-        }
-    }).then(function (accounts) {
-        var account = accounts;
-
-        db.Transaction.create({
-            Accounts_AccountID: req.body.Accounts_AccountID,
-            amount: req.body.amount
-        }).then(function (dbTransactions) {
-            var transaction = dbTransactions;
-            var acctBal = account.balance - dbTransactions.amount;
-
-            // update accounts balance here
-            db.Account.update(acctBal).then(function (currentBalance) {
-                var obj = {
-                    balance: currentBalance,
-                    transaction: transaction
-                }
-                res.json(obj);
-            });
-        });
-
-
     })
 
 
-});
+
+
+    // GET route for accounts 
+    app.get("/api/accounts", function (req, res) {
+        db.Account.findAll({ include: [db.Account] })
+            .then(function (dbAccounts) {
+                res.json(dbAccounts);
+            });
+    });
+
+    // GET route for single account
+    app.get("/api/accounts/:User_ID", function (req, res) {
+        db.Account.findOne({
+            where: {
+                User_ID: req.params.User_ID
+            }
+        }).then(function (dbAccounts) {
+            res.json(dbAccounts);
+        });
+    });
+
+    // Put routes for updating account
+    app.put("/api/accounts/:User_UserID", function (req, res) {
+        db.Account.findOne({
+            where: {
+                User_ID: req.params.User_ID
+            }
+        }).then(function (thisAccount) {
+
+            db.Account.update(req.body, thisAccount
+            ).then(function (dbAccounts) {
+                res.json(dbAccounts);
+            });
+
+        })
+
+    });
+
+    // Get route for all transactions
+    app.get("/api/transactions", function (req, res) {
+        db.Transaction.findAll({ include: [db.Transaction] })
+    }).then(function (allTransactions) {
+        res.json(allTransactions);
+    });
+
+    // Get route for one transactions
+    app.get("/api/transactions/:transactions_ID", function (req, res) {
+        db.transaction.findOne({
+            where: {
+                transactions_ID: req.params.transaction_ID
+            }
+        }).then(function (oneTransaction) {
+            res.json(oneTransaction);
+        });
+    });
+
+
+    // Post route for transactions
+    app.post("/api/transactions", function (req, res) {
+
+        db.Account.findOne({
+            where: {
+                User_ID: req.params.User_ID
+            }
+        }).then(function (accounts) {
+            var account = accounts;
+
+            db.Transaction.create({
+                Accounts_AccountID: req.body.Accounts_AccountID,
+                amount: req.body.amount
+            }).then(function (dbTransactions) {
+                var transaction = dbTransactions;
+                var acctBal = account.balance - dbTransactions.amount;
+
+                // update accounts balance here
+                db.Account.update(acctBal).then(function (currentBalance) {
+                    var obj = {
+                        balance: currentBalance,
+                        transaction: transaction
+                    }
+                    res.json(obj);
+                });
+            });
+
+
+        })
+
+
+    });
 
 }
